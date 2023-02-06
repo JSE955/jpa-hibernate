a070dacfb68d384dc1f3b7df5b0b4503cf9b565e
JEdwards <jordanscottedwards@gmail.com>
2023-02-06 10:03:24 -0500

    Set up H2 and created sql schema

--- src/main/java/com/in28minutes/springboot/learnjpaandhibernate/LearnJpaAndHibernateApplication.java
+++ src/main/java/com/in28minutes/springboot/learnjpaandhibernate/LearnJpaAndHibernateApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LearnJpaAndHibernateApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LearnJpaAndHibernateApplication.class, args);
 	}
-
+	
 }
