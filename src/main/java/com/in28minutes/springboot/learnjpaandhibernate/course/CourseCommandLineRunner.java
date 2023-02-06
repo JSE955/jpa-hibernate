a0ff181d51352be8ba66a3151f8c6edc6c8fd3b3
JEdwards <jordanscottedwards@gmail.com>
2023-02-06 10:58:02 -0500

    Implemented JPA for sql queries

--- src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -1,21 +1,21 @@
-package com.in28minutes.springboot.learnjpaandhibernate.course.jdbc;
+package com.in28minutes.springboot.learnjpaandhibernate.course;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
+import com.in28minutes.springboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
 
 @Component
-public class CourseJdbcCommandLineRunner implements CommandLineRunner {
+public class CourseCommandLineRunner implements CommandLineRunner {
 	
 	@Autowired
-	private CourseJdbcRepository repository;
+	private CourseJpaRepository repository;
 	
 	@Override
 	public void run(String... args) throws Exception {
-		repository.insert(new Course(1, "Learn AWS", "in28minutes"));
-		repository.insert(new Course(2, "Learn Azure", "in28minutes"));
+		repository.insert(new Course(1, "Learn Jpa", "in28minutes"));
+		repository.insert(new Course(2, "Learn Azure Jpa", "in28minutes"));
 		repository.insert(new Course(3, "Learn DevOps", "in28minutes"));
 		repository.deleteById(1);
 		System.out.println(repository.findById(2));
